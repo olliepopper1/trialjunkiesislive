6043d4bb1e4a4d5495e191791620b326f2a0ee55
olliepopper1 <aiagentmemes@gmail.com>
2025-02-03 10:02:36 +0000

    commit

--- tests/config/setup.cjs
+++ tests/config/setup.cjs
@@ -1,7 +1,8 @@
 'use strict';
 
 const chai = require('chai');
-const mocha = require('mocha');
+const { before, after, beforeEach, afterEach } = require('mocha');
+const { JSDOM } = require('jsdom');
 
 // Make Mocha functions available globally
 Object.assign(global, mocha);
@@ -13,10 +14,28 @@ global.expect = chai.expect;
 process.env.NODE_ENV = 'test';
 process.env.JWT_SECRET = 'test-secret';
 
+// Setup JSDOM
+const dom = new JSDOM('<!DOCTYPE html><html><body></body></html>', {
+    url: 'http://localhost'
+});
+
+global.window = dom.window;
+global.document = dom.window.document;
+global.navigator = dom.window.navigator;
+
 // Server cleanup
 afterEach(async () => {
   const app = require('../../src/app.cjs');
   if (app && app.server) {
     await new Promise((resolve) => app.server.close(resolve));
   }
+});
+
+// Additional setup and cleanup
+beforeEach(() => {
+    // Setup code
+});
+
+afterEach(() => {
+    // Cleanup code
 });
