7f3dcac1fca519a1cae07dce7f668787846f4393
olliepopper1 <aiagentmemes@gmail.com>
2025-02-03 04:38:35 +0000

    Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   .github/workflows/ci.yml #	modified:   jest.config.js #

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,4 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  moduleFileExtensions: ['ts', 'js', 'json'],
 };
